1b1e10c9e9dd52f5e8a334cb68ebafd186ba52c6
Diego Guerrero <diego.guerrero02@uptc.edu.co>
2021-08-22 00:06:33 -0500

    se agregaron artistas a cada evento

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import Presale from '../models/Presale';
+import Artist from '../models/Artist';
 
 const EventModel = new Schema ({
     id_event: {type: Number, required: true, unique: true},
@@ -7,7 +8,8 @@ const EventModel = new Schema ({
     city_event: {type: String, required: true},
     direction_event: {type: String, required: false},
     description_event: {type: String, unique: true, maxlength: 1000, required: false},
-    presale: [{ref:Presale, type: Schema.Types.ObjectId, require: false}],
+    presale: [{ref:Presale, type: Schema.Types.ObjectId, require: true}],
+    artists: [{ref:Artist, type: Schema.Types.ObjectId, require: true}],
     flyer: {type: String, required: false}
 }, {
     timestamps: true,
